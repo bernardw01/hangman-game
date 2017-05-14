4e2598b94c5c9d1b6b7dc5e6dbff6aeec54f74c7
Bernard Williams <bernard.l.williams@live.com>
2017-05-13 21:44:16 -0400

    First version of the working solution.

--- assets/javascript/config.js
+++ assets/javascript/config.js
@@ -11,23 +11,23 @@ function getConfig(itemNum) {
   var config = {
     words: [
       {
-        word: "Awkward",
+        word: "awkward",
         hint: "When someone walks into a room and hears you talking crap about them.  That situation is ..."
       },
       {
-        word: "Jazzy Jeff",
+        word: "jazzy jeff",
         hint: "This DJ hangs with a fresh prince."
       },
       {
-        word: "Oxygen",
+        word: "oxygen",
         hint: "This element puts the O in O2"
       },
       {
-        word: "Yacht",
+        word: "yacht",
         hint: "Its bigger than a boat and smaller than a ship."
       },
       {
-        word: "Zombie",
+        word: "zombie",
         hint: "This late night muncher loves smart people with brains."
       }
     ]
--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -12,20 +12,26 @@ var txtInstructions = document.getElementById("instructions");
 var txtPerfMetrics = document.getElementById("perfMetrics");
 var txtOverallAvg = document.getElementById("overallAvg");
 var imgHangManState = document.getElementById("hangManState");
+var txtCountRight = document.getElementById("countRight");
+var txtCountWrong = document.getElementById("countWrong");
+var txtCountRemaining = document.getElementById("countRemaining");
 
 //Variable to hold the choices that have been submitted
 var prevChoices = [];
 
-//Variable that tracks how many wrong answers the user has submitted
-var intHangState = 0;
-
 //Variable that holds the word spliced into multiple cells
 var wordArray = [];
+
+//Variable that holds the current letters and dashes indicating which letters you have hit
+var displayArray = [];
 
 //Variable that holds the length of the word the computer selected
 var wordLength = 0;
 var choiceCount = 0;
 var theCurrentWord = "";
+var countRight = 0;
+var countWrong = 0;
+var countRemaining = 6;
 
 //This function returns a new word from the config.js file.
 function getNewWord() {
@@ -40,6 +46,8 @@ function getNewWord() {
 
   txtWordHint.textContent = words[rndmCount].hint;
 
+  //Add the letters of the word individiually into an array
+  wordArray = words[rndmCount].word.split("");
   return words[rndmCount];
 }
 
@@ -58,8 +66,9 @@ function gameInit() {
 
   //Fill the box with dashes
   for (var index = 0; index < theCurrentWord.length; index++) {
-    txtTheWord.textContent += "-";
+    displayArray.push("-");
   }
+  txtTheWord.textContent = displayArray.join("");
 }
 
 //this function resets the graphics on the game board
@@ -77,21 +86,52 @@ document.onkeyup = function(event) {
 
 //This code handles the response that the user has typed in and checks to see if it is accurate
 function handleChoice(userGuess) {
-  prevChoices.push(userGuess);
-  txtPrevChoices.textContent += userGuess + " ";
-  choiceCount++;
-  checkChoice(userGuess);
+  //check to see if the user has already selected this letter
+  if (prevChoices.indexOf(userGuess) < 0) {
+    //Add the choice to the previously selected list and display
+    prevChoices.push(userGuess);
+    txtPrevChoices.textContent += userGuess + " ";
+    choiceCount++;
+
+    //Is this choice right or wrong?
+    if (checkChoice(userGuess)) {
+      countRight++;
+      txtCountRight.textContent = countRight;
+    } else {
+      //If the user guesses a wrong answer increment the wrong answer count and change the image
+      countWrong++;
+      txtCountWrong.textContent = countWrong;
+      imgHangManState.setAttribute("src", "assets/images/hangman-" + (countWrong + 1) + ".png");
+    }
+
+    
+  }
 }
 
 function checkChoice(userGuess) {
   rightChoice = false;
-  if (theCurrentWord.search(userGuess) > 0) {
+
+  //Location where the letter was found
+  var indices = [];
+
+  //Check to see if the letter is used in the random word,
+  //if so update the corresponding cell in the displayArray and update the page
+  var idx = wordArray.indexOf(userGuess);
+
+  while (idx != -1) {
+    //indices.push(idx);
     rightChoice = true;
+    displayArray[idx] = userGuess;
+    idx = wordArray.indexOf(userGuess, idx + 1);
   }
+
+  txtTheWord.textContent = displayArray.join("");
 
   console.log("--------Check choice-------------");
   console.log("rightchoice = " + rightChoice);
   console.log(theCurrentWord.search(userGuess));
+  console.log(theCurrentWord);
   console.log("------------------------------------");
+
   return rightChoice;
 }
